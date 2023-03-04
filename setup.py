f2b834e3f6ea06cdc078643b488537b5829d908b
tensorflow-project <126180162+tensorflow-project@users.noreply.github.com>
2023-03-04 16:05:29 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stable-diffusion', # replace with the name of your package
+    version='0.1', # replace with the version of your package
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='Description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='hhttps://github.com/tensorflow-project/stable-diffusion',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
