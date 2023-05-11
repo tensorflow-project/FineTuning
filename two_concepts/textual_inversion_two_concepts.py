21f3d92ebe66571770685dec8c0fa8cc115a3356
tensorflow-project <126180162+tensorflow-project@users.noreply.github.com>
2023-05-11 12:31:52 +0200

    Update textual_inversion_two_concepts.py

--- two_concepts/textual_inversion_two_concepts.py
+++ two_concepts/textual_inversion_two_concepts.py
@@ -38,10 +38,6 @@ py_file_location = "/content/FineTuning/models"
 sys.path.append(os.path.abspath(py_file_location))
 
 ### import the different models from our Github repository
-#from text_encoder import TextEncoder
-#from decoder import Decoder
-#from diffusion_model import DiffusionModel
-#from stable_diffusion import StableDiffusion
 from models.text_encoder import TextEncoder
 from models.decoder import Decoder
 from models.diffusion_model import DiffusionModel
